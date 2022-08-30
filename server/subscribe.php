c67ab8431fc3c44f1c7dbfe613a0de211aabdaed
Maxim Boltenkov <e1ement79@inbox.ru>
2022-08-30 23:09:37 +0300

    subscribe api #0.7

--- server/subscribe.php
+++ server/subscribe.php
@@ -1,32 +1,37 @@
 ﻿<?php
-if ($_POST['email']) {
-  $dbhost = '';
-  $dbuser = '';
-  $dbpass = '';
-  $dbname = '';
-  $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  include_once 'config.php';
 
-  if($mysqli->connect_errno) {
-    printf("Connect failed: %s<br />", $mysqli->connect_error);
-    exit();
+  if ($_POST['email']) {
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+    if($mysqli->connect_errno) {
+      printf("Connect failed: %s<br />", $mysqli->connect_error);
+      exit();
+    }
+
+    // date('Y-m-d H:i:s')
+    $insQuery = "INSERT INTO `subscriptions` (`email`, `status`)" .
+      " VALUES ('" .$_POST['email'] . "', 'active')";
+
+    if ($mysqli->query($insQuery)) {
+      $id = $mysqli->insert_id;
+      $res = $mysqli->query("SELECT * FROM `subscriptions` WHERE `id` = " . $id);
+      $row = $res->fetch_row();
+      $result = [
+        'success' => true,
+        'data' => $row,
+        'message' => 'OK'
+      ];
+      $res->free_result();
+    } else if ($mysql->errno) {
+      $result = [
+        'success' => false,
+        'message' => $mysqli->error
+      ];
+    }
+
+    $mysqli->close();
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($result);
   }
-
-  $query = "INSERT INTO `subscriptions` (`email`, `date`) VALUES ('" . $_POST['email'] . "', '" . date('Y-m-d H:i:s') . "')";
-  if ($mysqli->query($query)) {
-    $result = [
-      'success' => true,
-      'data' => [],
-      'message' => 'OK'
-    ];
-  } else if ($mysql->errno) {
-    $result = [
-      'success' => false,
-      'message' => $mysqli->error
-    ];
-  }
-
-  $mysqli->close();
-  header('Content-Type: application/json; charset=utf-8');
-  echo json_encode($result);
-}
 ?>
