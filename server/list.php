d9cacd24522e7770bd0c1b53f3adeb9b12dbe880
Maxim Boltenkov <e1ement79@inbox.ru>
2022-08-31 15:44:22 +0300

    api: list susbcriptions #0.1

---
+++ server/list.php
@@ -0,0 +1,23 @@
+﻿<?php
+  include_once 'config.php';
+
+  $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+  if($mysqli->connect_errno) {
+    printf("Connect failed: %s<br />", $mysqli->connect_error);
+    exit();
+  }
+
+  echo '<h1>Подписчики</h1>';
+  $res = $mysqli->query('SELECT * FROM `' . $tableName);
+  echo '<table border="1">';
+  while ($row = $res->fetch_row()) {
+    echo '<tr>' . '<td>' . $row[0] . '</td><td>' . $row[1] . '</td><td>' . $row[2] .
+      '</td><td>' . $row[3] . '</td></tr>'; 
+  }
+  echo '</table>';
+
+  $mysqli->close();
+  // header('Content-Type: application/json; charset=utf-8');
+  // echo json_encode($result);
+?>
