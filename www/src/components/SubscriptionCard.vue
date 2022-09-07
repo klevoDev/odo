f44f2e7e6fc7800a284119c06d140074a5eb8f48
Maxim Boltenkov <e1ement79@inbox.ru>
2022-09-07 18:59:12 +0300

    added subscription card

---
+++ www/src/components/SubscriptionCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="page__subscribe-pop subscribe-pop">
+    <div class="container">
+      <div class="subscribe-pop__column">
+        <div class="subscribe-pop__item">
+          <div class="subscribe-pop__wrap-pop-img">
+            <img class="subscribe-pop__pop-img" src="./img/subscribe-pop.png" alt="A person writes a message.">
+          </div>
+          <div class="subscribe-pop__body">
+            <h3 class="subscribe-pop__title">
+              Подписка
+            </h3>
+            <p class="subscribe-pop__text">
+              Пришлем уведомление на почту о новом оффере :)
+            </p>
+            <p class="subscribe-pop__topic">
+              Каккие события интересуют?
+            </p>
+            <ul class="subscribe-pop__list">
+              <li class="subscribe-pop__point">
+                Frontend-разработчик
+              </li>
+              <li class="subscribe-pop__point">
+                Python-разработчик
+              </li>
+              <li class="subscribe-pop__point">
+                Java-разработчик
+              </li>
+              <li class="subscribe-pop__point">
+                Тестировщик ПО
+              </li>
+              <li class="subscribe-pop__point">
+                Android-разработчик
+              </li>
+              <li class="subscribe-pop__point">
+                iOS-разработчик
+              </li>
+              <li class="subscribe-pop__point">
+                Frontend
+              </li>
+              <li class="subscribe-pop__point">
+                Backend
+              </li>
+              <li class="subscribe-pop__point">
+                Android
+              </li>
+              <li class="subscribe-pop__point">
+                iOS
+              </li>
+              <li class="subscribe-pop__point">
+                ML
+              </li>
+              <li class="subscribe-pop__point">
+                QA
+              </li>
+            </ul>
+            <p class="subscribe-pop__topic">
+              E-mail
+            </p>
+            <form class="subscribe-pop__form" action="#">
+              <div class="subscribe-pop__wrap-input">
+                <input class="subscribe-pop__input" type="text" placeholder="" name="email">
+              </div>
+            </form>
+            <div class="subscribe-pop__wrap-buttons">
+              <div class="subscribe-pop__write-telegram">
+                <a class="subscribe-pop__link" href="#" target="_blank">
+                  Подписаться
+                </a>
+              </div>
+              <div class="subscribe-pop__close-button">
+                Закрыть
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
