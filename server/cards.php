5a25ffbc71b92d350ca938290be40d56eb833fe2
Maxim Boltenkov <e1ement79@inbox.ru>
2022-09-17 01:20:17 +0300

    use db for fetching cards (init)

--- server/cards.php
+++ server/cards.php
@@ -1,5 +1,23 @@
 <?php
-  include_once 'data.php';
+  include_once 'config.php';
+  // include_once 'data.php';
+
+  $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+  if($mysqli->connect_errno) {
+    printf("Connect failed: %s<br />", $mysqli->connect_error);
+    exit();
+  }
+
+  $cards = [];
+  $tableName = 'odo_cards';
+  $res = $mysqli->query('SELECT * FROM `' . $tableName . '`');
+  while ($row = $res->fetch_row()) {
+    $cards[] = $row;
+  }
+
+  $res->free_result();
+  $mysqli->close();
 
   $result = [
     'success' => true,
